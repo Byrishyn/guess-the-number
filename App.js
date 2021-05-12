74bc4b441b41e11c85a8d42dd2503367b7b84af1
Leroux Antoine <antoine.d.m.leroux@gmail.com>
2021-05-12 21:08:52 +0900

    Add GameScreen to App

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import Header from "./components/Header"
 import StartGameScreen from "./screens/StartGameScreen"
+import GameScreen from "./screens/GameScreen"
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  const gameStartHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber)
+  }
+
+  let content = <StartGameScreen onStartGame ={gameStartHandler}/>
+
+  if (userNumber){
+    content = <GameScreen userChoice={userNumber}/>
+  }
+
+
   return (
     <View style={styles.screen}>
       <Header title="Guess a number" />
-      <StartGameScreen/>
+      {content}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  screen : {
+  screen: {
     flex: 1,
   }
 });
