a1e4173ff06c1d87ac2c7214354dc8ccafdaba87
Leroux Antoine <antoine.d.m.leroux@gmail.com>
2021-05-14 23:26:36 +0900

    add restart game

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
 
 const GameOverScreen = props => {
     return (
         <View style={styles.screen} >
             <Text>The game is over !</Text>
+            <Text>Number of rounds : {props.numberRounds}</Text>
+            <Text>The number was : {props.userNumber}</Text>
+            <Button title="New Game" onPress={props.onNewGame} />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    screen : {
-        flex:1,
-        alignItems:"center",
+    screen: {
+        flex: 1,
+        alignItems: "center",
         justifyContent: "center",
     }
 })
