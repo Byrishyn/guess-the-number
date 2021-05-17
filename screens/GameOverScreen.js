6bf1bc574f2e52d344df37ce5a0080def26c94b7
Leroux Antoine <antoine.d.m.leroux@gmail.com>
2021-05-17 20:56:53 +0900

    Add image to GameoOVerScreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { View, Image, StyleSheet, Button } from 'react-native'
+
+import BodyText from "../components/BodyText"
+import TitleText from "../components/TitleText"
 
 const GameOverScreen = props => {
     return (
         <View style={styles.screen} >
-            <Text>The game is over !</Text>
-            <Text>Number of rounds : {props.numberRounds}</Text>
-            <Text>The number was : {props.userNumber}</Text>
+            <TitleText>The game is over !</TitleText>
+            <Image source={require("../assets/success.png")}/>
+            <BodyText>Number of rounds : {props.numberRounds}</BodyText>
+            <BodyText>The number was : {props.userNumber}</BodyText>
             <Button title="New Game" onPress={props.onNewGame} />
         </View>
     )
