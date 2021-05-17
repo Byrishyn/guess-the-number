feb6266bad1aa94d04694ed055a7c0e638baa734
Leroux Antoine <antoine.d.m.leroux@gmail.com>
2021-05-17 13:02:44 +0900

    Add default styles and default component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,6 +5,8 @@ import Card from "../components/Card";
 import Input from "../components/Input"
 import NumberContainer from "../components/NumberContainer"
 import Colors from "../constants/colors";
+import TitleText from "../components/TitleText"
+import BodyText from "../components/BodyText"
 
 const StartGameScreen = props => {
   const [enteredValue, setEnteredValue] = useState("")
@@ -49,9 +51,9 @@ const StartGameScreen = props => {
       Keyboard.dismiss();
     }} >
       <View style={styles.screen}>
-        <Text style={styles.title}>Start a New Game !</Text>
+        <TitleText style={styles.title}>Start a New Game !</TitleText>
         <Card style={styles.inputContainer}>
-          <Text>Select a Number</Text>
+          <BodyText>Select a Number</BodyText>
           <Input
             style={styles.input}
             blurOnSubmit
