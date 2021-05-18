2a1f5e6590c10e8629cef4421ac93ea95a22f9fc
Leroux Antoine <antoine.d.m.leroux@gmail.com>
2021-05-18 10:51:19 +0900

    change image source to web image

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -9,7 +9,11 @@ const GameOverScreen = props => {
         <View style={styles.screen} >
             <TitleText>The game is over !</TitleText>
             <View style={styles.imageContainer}>
-                <Image source={require("../assets/success.png")} style={styles.image}/>
+                <Image
+                    //source={require("../assets/success.png")}
+                    source={{uri:"https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_960_720.jpg"}}
+                    style={styles.image}
+                />
             </View>
             <BodyText>Number of rounds : {props.numberRounds}</BodyText>
             <BodyText>The number was : {props.userNumber}</BodyText>
