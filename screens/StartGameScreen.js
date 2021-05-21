76a50b40222ceef91363336216f5445519d331da
Leroux Antoine <antoine.d.m.leroux@gmail.com>
2021-05-21 18:24:17 +0900

    add Dimension component to StartGameScreem

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Text, Button, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
+import { View, StyleSheet, Text, Button, TouchableWithoutFeedback, Keyboard, Alert, Dimensions } from 'react-native';
 
 import Card from "../components/Card";
 import Input from "../components/Input"
@@ -104,7 +104,7 @@ const styles = StyleSheet.create({
     fontFamily: "open-sans-bold"
   },
   button: {
-    width: 100
+    width: Dimensions.get("window").width /4
   },
   input: {
     width: 50,
